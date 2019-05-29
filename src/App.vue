131e8de0725b3f546fbeabff5a5ae04e205f6153
Roberto <bronzetto@outlook.com>
2019-05-29 12:36:59 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="flash">FRAMEWORKS COMPARISON</h1>
+    <div class="menu-buttons-">
+      <button class="button-fr" @click.prevent="setFramework('vuetify')">Vuetify</button>
+      <button class="button-fr" @click.prevent="setFramework('element')">Element</button>
+      <button class="button-fr" @click.prevent="setFramework('syncfusion')">Syncfusion</button>
+      {{ currentFramework }}
+    </div>
+
+    <div class="changing-frameworks">
+      <div class="vuetify" v-if="currentFramework === 'vuetify'">
+        <vuetifyTemp v-bind:dataTable="tableData" v-if="tableData.length > 0"/>
+      </div>
+
+      <div v-bind:dataTable="tableData" class="element" v-if="currentFramework === 'element'">
+        <elementTemp v-bind:dataTable="tableData" v-if="tableData.length > 0"/>
+      </div>
+
+      <div v-bind:dataTable="tableData" class="syncfusion" v-if="currentFramework === 'syncfusion'">
+        <SyncfusionTemp v-bind:dataTable="tableData" v-if="tableData.length > 0"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vuetifyTemp from "./components/vuetifyTemp.vue";
+import elementTemp from "./components/elementTemp.vue";
+import SyncfusionTemp from "./components/syncfusionTemp.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    vuetifyTemp,
+    elementTemp,
+    SyncfusionTemp
+  },
+  data() {
+    return {
+      currentFramework: "vuetify",
+      tableData: []
+    };
+  },
+  computed: {
+    getData: function() {
+      axios
+        .get('https://www.googleapis.com/books/v1/volumes?q=""')
+        .then(result => (this.tableData = result.data.items))
+        .catch(err => console.error(err));
+    }
+  },
+  methods: {
+    setFramework: function(framework) {
+      this.currentFramework = framework;
+    }
+  },
+  mounted() {
+    this.getData;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button.button-fr {
+  border: 2px solid gray;
+  margin: 5px;
+  padding: 10px;
+}
+
+@-webkit-keyframes flash {
+  from,
+  50%,
+  to {
+    background-color: rgb(255, 255, 255);
+  }
+  25%,
+  75% {
+    background-color: rgb(253, 18, 18);
+  }
+}
+
+@keyframes flash {
+  from,
+  50%,
+  to {
+    background-color: rgb(255, 255, 255);
+  }
+  25%,
+  75% {
+    background-color: rgb(253, 18, 18);
+  }
+}
+
+.flash {
+  -webkit-animation-duration: 1s;
+  animation-duration: 1s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation-name: flash;
+  animation-name: flash;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import ElementUI from "element-ui";
+import "vuetify/dist/vuetify.min.css";
+import "element-ui/lib/theme-chalk/index.css";
+import { GridPlugin } from "@syncfusion/ej2-vue-grids";
 
-Vue.config.productionTip = false
+Vue.use(GridPlugin);
+
+Vue.use(ElementUI);
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
